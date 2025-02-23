c6458c4fae94d5605e0d2b2217c101e68b570158
ice breaker <1324318532@qq.com>
2025-02-23 18:47:18 +0800

    chore(deps): upgrade

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const { UnifiedWebpackPluginV5 } = require('weapp-tailwindcss/webpack')
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+const config = {
+  // some option...
+  configureWebpack: (config) => {
+    config.plugins.push(
+      new UnifiedWebpackPluginV5({
+        appType: 'uni-app'
+      })
+    )
+  }
+  // other option...
+}
+
+module.exports = config
